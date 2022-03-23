19c2b2104312a64c7704aa82680cdbae98b22d76
Ramen Manna <ramen209admit@gmail.com>
2022-03-23 16:17:05 +0530

    commited by ramen

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const ejs = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
-
+//Assests
+app.use(express.static('public'))
 app.get('/',(req, res)=>{
     res.render('home')
 })
